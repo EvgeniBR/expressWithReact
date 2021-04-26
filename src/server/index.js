3973392053e8946866806b6a867bc26a5bcfeab9
EvgeniBR <genib1989@gmail.com>
2021-04-26 11:27:33 +0300

    a

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const os = require('os');
+const path = require('path');
 
 const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 8080;
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.listen(port, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
